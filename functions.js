b87fd88ad8d281583f00fac873c8aea967d023c6
jsm1306 <sindhu.j1729@gmail.com>
2024-12-10 20:12:14 +0530

    Practiced Functions

--- functions.js
+++ functions.js
@@ -28,3 +28,55 @@ Array.forEach(function printval(val,idx){
 Array.forEach((val,idx)=>{
     console.log(val,idx)
 })
+// Array.forEach((val)=>{
+// console.log(val*val)
+// })
+
+let calcsquare = (val)=>{
+    console.log(val*val);
+}
+Array.forEach(calcsquare)
+
+//map function
+Array.map(val=>console.log(val))
+
+thank = [20,30,60,45,0,32.2]
+let newthank = thank.map(val=>{return val+179})
+console.log(newthank)
+
+//to filter out even numbers from an array
+let evenarr = thank.filter(val=>val%2===0)
+console.log(evenarr)
+let evenar1r = thank.filter(val=>{
+    return val%2==0;
+})
+console.log(evenar1r)
+
+//reduce function is used to reduce the array to a single value
+let sum1 = thank.reduce((acc,val)=>{
+    return acc>val?acc:val;
+})
+// accumulator + current value = new accumulator
+console.log(sum1)
+
+//marks of students
+studentmarks = [40,55,93,97,21,88,69,78,85,90]
+let topmarks = studentmarks.filter(val=>{
+    return val>90
+})
+
+console.log(topmarks)
+let userinput = prompt("Enter a number")
+array2 = []
+for( let i=1;i<=userinput;i++){
+    array2.push(i)
+}
+console.log("The array is: ",array2)
+let sumarray = array2.reduce((acc,val)=>{
+    return acc+val;
+})
+console.log("Sum of the array is: ",sumarray)
+let productarray = array2.reduce((acc,val)=>{
+    return acc*val;
+})
+console.log("Product of the array is: ",productarray)
